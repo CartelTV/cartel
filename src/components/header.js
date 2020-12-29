fc8743041d8a3b5840848daa3c98159ceeaf6838
Mike Riley <mikeriley131@gmail.com>
2020-12-28 23:10:31 -0700

    initial commit and header

---
+++ src/components/header.js
@@ -0,0 +1,122 @@
+import React, { useEffect, useRef, useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+import { useWindowWidth } from '@react-hook/window-size';
+
+import { debounce, handleNavVisibility } from '../util';
+
+import cartelTextLogo from '../images/cartel-text-logo.png';
+
+export const Header = ({ siteTitle }) => {
+  const [menuIsExpanded, setMenuIsExpanded] = useState(false);
+  const menuRef = useRef();
+  const windowWidth = useWindowWidth();
+
+  const toggleMenu = () => {
+    setMenuIsExpanded(!menuIsExpanded);
+  };
+
+  useEffect(() => {
+    const handleEscPress = event => {
+      if (event.key === 'Escape' || event.keyCode === 27) {
+        setMenuIsExpanded(!menuIsExpanded);
+      }
+    };
+
+    const handleOutsideClick = event => {
+      if (menuIsExpanded) {
+        // ignore clicks on the component itself
+        if (menuRef.current.contains(event.target)) {
+          return;
+        }
+        setMenuIsExpanded(false);
+      }
+    };
+
+    if (windowWidth > 767) setMenuIsExpanded(false);
+
+    // close menu dropdown on ESC
+    window.addEventListener('keydown', handleEscPress);
+
+    // handle click outside of component
+    document.addEventListener('click', handleOutsideClick, false);
+
+    // handle showing/hiding nav on scroll up/down
+    const nav = document.querySelector('.header__wrapper');
+    window.addEventListener('scroll', () =>
+      debounce(handleNavVisibility(nav), 1000)
+    );
+
+    return () => {
+      window.removeEventListener('keydown', handleEscPress);
+      window.removeEventListener('scroll', handleNavVisibility);
+      document.removeEventListener('click', handleOutsideClick, false);
+    };
+  }, [menuIsExpanded]);
+
+  return (
+    <header className="header" role="banner" ref={menuRef}>
+      <button
+        className="header__menu-button"
+        type="button"
+        aria-label="open site navigation"
+        aria-controls="nav-list"
+        aria-expanded={menuIsExpanded}
+        onClick={toggleMenu}
+      >
+        {menuIsExpanded ? 'Close' : 'Menu'}
+      </button>
+      <nav
+        className="nav"
+        role="navigation"
+        id="nav-list"
+        hidden={windowWidth > 767 ? false : !menuIsExpanded}
+      >
+        <ul className="nav__list">
+          <li className="nav__item">
+            <Link to="/" className="nav__link">
+              Work
+            </Link>
+          </li>
+          <li className="nav__item">
+            <Link to="/" className="nav__link">
+              Editors
+            </Link>
+          </li>
+          <li className="nav__item">
+            <Link to="/" className="nav__link">
+              Contact
+            </Link>
+          </li>
+          <li className="nav__item">
+            <Link to="/" className="nav__link">
+              Music Videos
+            </Link>
+          </li>
+          <li className="nav__item">
+            <Link to="/" className="nav__link">
+              The Lookout
+            </Link>
+          </li>
+          <li className="nav__item">
+            <Link to="/" className="nav__link">
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Link to="/">
+        <h1 className="visuallyhidden">{siteTitle}</h1>
+        <img className="header__logo" src={cartelTextLogo} alt="Cartel logo" />
+      </Link>
+    </header>
+  );
+};
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+};
+
+Header.defaultProps = {
+  siteTitle: ``,
+};
---
+++ src/components/layout.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
+
+import '../styles/main.scss'
+
+import { Header } from './header'
+
+export const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <React.Fragment>
+        <a className="skip-link" href="#main">
+          skip to main content
+        </a>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <main className="main" id="main" role="main">
+          {children}
+        </main>
+      </React.Fragment>
+    )}
+  />
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}

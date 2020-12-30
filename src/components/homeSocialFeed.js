4368672c9a649e1a09c18c2b0ced87cd0ef00c7f
Mike Riley <mikeriley131@gmail.com>
2020-12-29 20:42:26 -0700

    homepage components

---
+++ src/components/homeSocialFeed.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const HomeSocialFeed = () => (
+  <article className="home-social-feed">
+    <h2 className="home-social-feed__heading">The Latest</h2>
+    <ul className="home-social-feed__list">
+      <li className="home-social-feed__item">
+        <img src="https://picsum.photos/id/102/396" alt="" />
+      </li>
+      <li className="home-social-feed__item">
+        <img src="https://picsum.photos/id/103/396" alt="" />
+      </li>
+      <li className="home-social-feed__item">
+        <img src="https://picsum.photos/id/104/396" alt="" />
+      </li>
+    </ul>
+  </article>
+);

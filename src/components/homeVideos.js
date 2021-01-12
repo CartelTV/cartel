85785f377aced165af9bafdc3f65887f7f807d4e
Mike Riley <mikeriley131@gmail.com>
2021-01-11 20:42:52 -0700

    video play functionality and styles

--- src/components/homeVideos.js
+++ src/components/homeVideos.js
@@ -1,28 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { Image } from './patterns/image';
 
-export const HomeVideos = ({ sectionHeading, videosList }) => (
-  <article className="home-videos">
-    <div className="home-videos__heading-wrapper">
-      <h2 className="home-videos__heading">{sectionHeading}</h2>
-    </div>
-    <ul className="home-videos__list">
-      {videosList.map(video => (
-        <li className="home-videos__item">
-          <div className="home-videos__video-wrapper">
-            <Image srcSmall={video.thumbnail} alt={video.title} lazyLoad />
-          </div>
-          <div className="home-videos__text-wrapper">
-            <p>{video.editor}</p>
-            <p>{video.title}</p>
-          </div>
-        </li>
-      ))}
-    </ul>
-  </article>
-);
+import cartelMarkLogo from '../images/cartel-logo.png';
+
+export const HomeVideos = ({ sectionHeading, videosList }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const thumbnailClick = index => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <article className="home-videos">
+      <div className="home-videos__heading-wrapper">
+        <h2 className="home-videos__heading">{sectionHeading}</h2>
+      </div>
+      <ul className="home-videos__list">
+        {videosList.map((video, index) => (
+          <li className="home-videos__item" key={video.videoSrc}>
+            <div className="home-videos__video-wrapper">
+              <div className="iframe-container iframe-container-16x9">
+                <iframe
+                  src={`${video.videoSrc}${
+                    activeIndex === index ? '&autoplay=1' : ''
+                  }`}
+                  title={video.title}
+                  frameBorder="0"
+                  allow="autoplay; fullscreen"
+                  allowFullScreen
+                />
+              </div>
+              <button
+                className="home-videos__button"
+                type="button"
+                onClick={() => thumbnailClick(index)}
+              >
+                <Image
+                  className={`home-videos__thumbnail ${
+                    activeIndex === index ? 'play-video' : ''
+                  }`}
+                  srcSmall={video.imageSrc}
+                  alt="Race Day is Still Sacred"
+                  lazyLoad
+                />
+                <img
+                  className="home-videos__thumbnail-icon"
+                  src={cartelMarkLogo}
+                  alt=""
+                />
+              </button>
+            </div>
+            <div className="home-videos__text-wrapper">
+              <p>{video.editor}</p>
+              <p>{video.title}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </article>
+  );
+};
 
 HomeVideos.propTypes = {
   sectionHeading: PropTypes.string.isRequired,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,176 +1,165 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link } from 'gatsby';
 
 import { Layout } from '../components/layout';
 import SEO from '../components/seo';
 import { Image } from '../components/patterns/image';
 
+import { HomeVideos } from '../components/homeVideos';
 import { LinkCalloutArrow } from '../components/icons/linkCalloutArrow';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1 className="visuallyhidden">Home</h1>
-    <article className="home-videos">
-      <div className="home-videos__heading-wrapper">
-        <h2 className="home-videos__heading">Recent Work</h2>
+const recentWorkVideos = [
+  {
+    videoSrc:
+      'https://cartel.wiredrive.com/?routekey=iframe-embed&token=300e1776e61b6928228e78cd005577ac&loop=0&controls=1&color=4EB7D4',
+    title: 'Race Day is Still Sacred',
+    editor: 'Track Smith',
+    imageSrc: 'https://picsum.photos/id/1011/1740/1184',
+  },
+  {
+    videoSrc:
+      'https://cartel.wiredrive.com/?routekey=iframe-embed&token=234f2dda2110b43e094f19fcde9ad4b0&loop=0&controls=1&color=4EB7D4',
+    title: 'Race Day is Still Sacred',
+    editor: 'Track Smith',
+    imageSrc: 'https://picsum.photos/id/1012/3973/2639',
+  },
+  {
+    videoSrc:
+      'https://cartel.wiredrive.com/?routekey=iframe-embed&token=3e2f6c9d1ebdf0816c75a7a52c5fda1c&loop=0&controls=1&color=4EB7D4',
+    title: 'Race Day is Still Sacred',
+    editor: 'Track Smith',
+    imageSrc: 'https://picsum.photos/id/1013/4256/2832',
+  },
+];
+
+const IndexPage = () => {
+  const thumbnailRef = useRef(null);
+
+  const thumbnailClick = () => {
+    console.log('thumbnailRef.current', thumbnailRef.current);
+    // thumbnailRef.current.classList.add('video-play');
+  };
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1 className="visuallyhidden">Home</h1>
+      <HomeVideos sectionHeading="Recent Work" videosList={recentWorkVideos} />
+
+      <article className="home-videos">
+        <div className="home-videos__heading-wrapper">
+          <h2 className="home-videos__heading">Music Videos</h2>
+        </div>
+        <ul className="home-videos__list">
+          <li className="home-videos__item">
+            <div className="home-videos__video-wrapper">
+              <img src="https://picsum.photos/id/1015/6000/4000" alt="" />
+            </div>
+            <div className="home-videos__text-wrapper">
+              <p>Track Smith</p>
+              <p>Race Day is Still Sacred</p>
+            </div>
+          </li>
+          <li className="home-videos__item">
+            <div className="home-videos__video-wrapper">
+              <img src="https://picsum.photos/id/1016/3844/2563" alt="" />
+            </div>
+            <div className="home-videos__text-wrapper">
+              <p>Track Smith</p>
+              <p>Race Day is Still Sacred</p>
+            </div>
+          </li>
+          <li className="home-videos__item">
+            <div className="home-videos__video-wrapper">
+              <img src="https://picsum.photos/id/1018/3914/2935" alt="" />
+            </div>
+            <div className="home-videos__text-wrapper">
+              <p>Track Smith</p>
+              <p>Race Day is Still Sacred</p>
+            </div>
+          </li>
+        </ul>
+      </article>
+
+      <article className="home-editors">
+        <h2 className="home-editors__heading">Editors</h2>
+        <ul className="home-editors__list">
+          <li className="home-editors__item">
+            <Link className="home-editors__link" to="/">
+              Adam Robinson
+            </Link>
+          </li>
+          <li className="home-editors__item">
+            <Link className="home-editors__link" to="/">
+              Andy McGraw
+            </Link>
+          </li>
+          <li className="home-editors__item">
+            <Link className="home-editors__link" to="/">
+              Chris Catanach
+            </Link>
+          </li>
+          <li className="home-editors__item">
+            <Link className="home-editors__link" to="/">
+              Ellie Johnson
+            </Link>
+          </li>
+          <li className="home-editors__item">
+            <Link className="home-editors__link" to="/">
+              Ernie Gilbert
+            </Link>
+          </li>
+          <li className="home-editors__item">
+            <Link className="home-editors__link" to="/">
+              Kevin Zimmerman
+            </Link>
+          </li>
+          <li className="home-editors__item">
+            <Link className="home-editors__link" to="/">
+              Kyle Valenta
+            </Link>
+          </li>
+          <li className="home-editors__item">
+            <Link className="home-editors__link" to="/">
+              Leo Scott
+            </Link>
+          </li>
+          <li className="home-editors__item">
+            <Link className="home-editors__link" to="/">
+              Paul O&apos;Reilly
+            </Link>
+          </li>
+          <li className="home-editors__item">
+            <Link className="home-editors__link" to="/">
+              Ting Poo
+            </Link>
+          </li>
+        </ul>
+      </article>
+
+      <div className="link-callout">
+        <Link className="link-callout__link" to="/work">
+          <span>Work</span>
+          <LinkCalloutArrow />
+        </Link>
       </div>
-      <ul className="home-videos__list">
-        <li className="home-videos__item">
-          <div className="home-videos__video-wrapper">
-            <Image
-              srcSmall="https://picsum.photos/id/1011/5472/3648"
-              alt="Race Day is Still Sacred"
-              lazyLoad
-            />
-          </div>
-          <div className="home-videos__text-wrapper">
-            <p>Track Smith</p>
-            <p>Race Day is Still Sacred</p>
-          </div>
-        </li>
-        <li className="home-videos__item">
-          <div className="home-videos__video-wrapper">
-            <Image
-              srcSmall="https://picsum.photos/id/1012/3973/2639"
-              alt="Race Day is Still Sacred"
-              lazyLoad
-            />
-          </div>
-          <div className="home-videos__text-wrapper">
-            <p>Track Smith</p>
-            <p>Race Day is Still Sacred</p>
-          </div>
-        </li>
-        <li className="home-videos__item">
-          <div className="home-videos__video-wrapper">
-            <Image
-              srcSmall="https://picsum.photos/id/1013/4256/2832"
-              alt="Race Day is Still Sacred"
-              lazyLoad
-            />
-          </div>
-          <div className="home-videos__text-wrapper">
-            <p>Track Smith</p>
-            <p>Race Day is Still Sacred</p>
-          </div>
-        </li>
-      </ul>
-    </article>
 
-    <article className="home-videos">
-      <div className="home-videos__heading-wrapper">
-        <h2 className="home-videos__heading">Music Videos</h2>
-      </div>
-      <ul className="home-videos__list">
-        <li className="home-videos__item">
-          <div className="home-videos__video-wrapper">
-            <img src="https://picsum.photos/id/1015/6000/4000" alt="" />
-          </div>
-          <div className="home-videos__text-wrapper">
-            <p>Track Smith</p>
-            <p>Race Day is Still Sacred</p>
-          </div>
-        </li>
-        <li className="home-videos__item">
-          <div className="home-videos__video-wrapper">
-            <img src="https://picsum.photos/id/1016/3844/2563" alt="" />
-          </div>
-          <div className="home-videos__text-wrapper">
-            <p>Track Smith</p>
-            <p>Race Day is Still Sacred</p>
-          </div>
-        </li>
-        <li className="home-videos__item">
-          <div className="home-videos__video-wrapper">
-            <img src="https://picsum.photos/id/1018/3914/2935" alt="" />
-          </div>
-          <div className="home-videos__text-wrapper">
-            <p>Track Smith</p>
-            <p>Race Day is Still Sacred</p>
-          </div>
-        </li>
-      </ul>
-    </article>
-
-    <article className="home-editors">
-      <h2 className="home-editors__heading">Editors</h2>
-      <ul className="home-editors__list">
-        <li className="home-editors__item">
-          <Link className="home-editors__link" to="/">
-            Adam Robinson
-          </Link>
-        </li>
-        <li className="home-editors__item">
-          <Link className="home-editors__link" to="/">
-            Andy McGraw
-          </Link>
-        </li>
-        <li className="home-editors__item">
-          <Link className="home-editors__link" to="/">
-            Chris Catanach
-          </Link>
-        </li>
-        <li className="home-editors__item">
-          <Link className="home-editors__link" to="/">
-            Ellie Johnson
-          </Link>
-        </li>
-        <li className="home-editors__item">
-          <Link className="home-editors__link" to="/">
-            Ernie Gilbert
-          </Link>
-        </li>
-        <li className="home-editors__item">
-          <Link className="home-editors__link" to="/">
-            Kevin Zimmerman
-          </Link>
-        </li>
-        <li className="home-editors__item">
-          <Link className="home-editors__link" to="/">
-            Kyle Valenta
-          </Link>
-        </li>
-        <li className="home-editors__item">
-          <Link className="home-editors__link" to="/">
-            Leo Scott
-          </Link>
-        </li>
-        <li className="home-editors__item">
-          <Link className="home-editors__link" to="/">
-            Paul O&apos;Reilly
-          </Link>
-        </li>
-        <li className="home-editors__item">
-          <Link className="home-editors__link" to="/">
-            Ting Poo
-          </Link>
-        </li>
-      </ul>
-    </article>
-
-    <div className="link-callout">
-      <Link className="link-callout__link" to="/work">
-        <span>Work</span>
-        <LinkCalloutArrow />
-      </Link>
-    </div>
-
-    <article className="scrollable-cards">
-      <h2 className="scrollable-cards__heading">The Latest</h2>
-      <ul className="scrollable-cards__list">
-        <li className="scrollable-cards__item">
-          <img src="https://picsum.photos/id/102/396" alt="" />
-        </li>
-        <li className="scrollable-cards__item">
-          <img src="https://picsum.photos/id/103/396" alt="" />
-        </li>
-        <li className="scrollable-cards__item">
-          <img src="https://picsum.photos/id/104/396" alt="" />
-        </li>
-      </ul>
-    </article>
-  </Layout>
-);
+      <article className="scrollable-cards">
+        <h2 className="scrollable-cards__heading">The Latest</h2>
+        <ul className="scrollable-cards__list">
+          <li className="scrollable-cards__item">
+            <img src="https://picsum.photos/id/102/396" alt="" />
+          </li>
+          <li className="scrollable-cards__item">
+            <img src="https://picsum.photos/id/103/396" alt="" />
+          </li>
+          <li className="scrollable-cards__item">
+            <img src="https://picsum.photos/id/104/396" alt="" />
+          </li>
+        </ul>
+      </article>
+    </Layout>
+  );
+};
 
 export default IndexPage;

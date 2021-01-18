35e9c41fa7287213ad134dffc41b8527b3e739b1
Mike Riley <mikeriley131@gmail.com>
2021-01-17 17:40:30 -0700

    adjust editors image

--- src/components/patterns/videoDetail.js
+++ src/components/patterns/videoDetail.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-import { Image } from './image';
 import { LinkCalloutArrow } from '../icons/linkCalloutArrow';
 
 export const VideoDetail = () => (
--- src/pages/editors.js
+++ src/pages/editors.js
@@ -3,6 +3,7 @@ import { Link } from 'gatsby';
 
 import { Layout } from '../components/layout';
 import SEO from '../components/seo';
+import { Image } from '../components/patterns/image';
 import { LinkCallout } from '../components/patterns/linkCallout';
 
 const VideoDetailPage = () => (
@@ -15,51 +16,111 @@ const VideoDetailPage = () => (
           <Link className="editors__link" to="/editor-detail">
             Adam Robinson
           </Link>
+          <Image
+            className="editors__image"
+            srcSmall="https://picsum.photos/300/179"
+            alt=""
+            aspectRatio="600:360"
+          />
         </li>
         <li className="editors__item">
           <Link className="editors__link" to="/editor-detail">
             Andy McGraw
           </Link>
+          <Image
+            className="editors__image"
+            srcSmall="https://picsum.photos/300/179"
+            alt=""
+            aspectRatio="600:360"
+          />
         </li>
         <li className="editors__item">
           <Link className="editors__link" to="/editor-detail">
             Chris Catanach
           </Link>
+          <Image
+            className="editors__image"
+            srcSmall="https://picsum.photos/300/179"
+            alt=""
+            aspectRatio="600:360"
+          />
         </li>
         <li className="editors__item">
           <Link className="editors__link" to="/editor-detail">
             Ellie Johnson
           </Link>
+          <Image
+            className="editors__image"
+            srcSmall="https://picsum.photos/300/179"
+            alt=""
+            aspectRatio="600:360"
+          />
         </li>
         <li className="editors__item">
           <Link className="editors__link" to="/editor-detail">
             Ernie Gilbert
           </Link>
+          <Image
+            className="editors__image"
+            srcSmall="https://picsum.photos/300/179"
+            alt=""
+            aspectRatio="600:360"
+          />
         </li>
         <li className="editors__item">
           <Link className="editors__link" to="/editor-detail">
             Kevin Zimmerman
           </Link>
+          <Image
+            className="editors__image"
+            srcSmall="https://picsum.photos/300/179"
+            alt=""
+            aspectRatio="600:360"
+          />
         </li>
         <li className="editors__item">
           <Link className="editors__link" to="/editor-detail">
             Kyle Valenta
           </Link>
+          <Image
+            className="editors__image"
+            srcSmall="https://picsum.photos/300/179"
+            alt=""
+            aspectRatio="600:360"
+          />
         </li>
         <li className="editors__item">
           <Link className="editors__link" to="/editor-detail">
             Leo Scott
           </Link>
+          <Image
+            className="editors__image"
+            srcSmall="https://picsum.photos/300/179"
+            alt=""
+            aspectRatio="600:360"
+          />
         </li>
         <li className="editors__item">
           <Link className="editors__link" to="/editor-detail">
             Paul O&apos;Reilly
           </Link>
+          <Image
+            className="editors__image"
+            srcSmall="https://picsum.photos/300/179"
+            alt=""
+            aspectRatio="600:360"
+          />
         </li>
         <li className="editors__item">
           <Link className="editors__link" to="/editor-detail">
             Ting Poo
           </Link>
+          <Image
+            className="editors__image"
+            srcSmall="https://picsum.photos/300/179"
+            alt=""
+            aspectRatio="600:360"
+          />
         </li>
       </ul>
     </article>

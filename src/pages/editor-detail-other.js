313cf9c38f5993a0459f51978a889e8b403322f3
Mike Riley <mikeriley131@gmail.com>
2021-05-06 21:11:17 -0600

    editor detail active sub-nav link

--- src/pages/editor-detail-other.js
+++ src/pages/editor-detail-other.js
@@ -9,78 +9,78 @@ const videosList = [
   {
     videoSrc:
       'https://cartel.wiredrive.com/?routekey=iframe-embed&token=300e1776e61b6928228e78cd005577ac&loop=0&controls=1&color=4EB7D4',
-    title: 'Race Day is Still Sacred',
-    client: 'Track Smith',
-    imageSrc: 'https://picsum.photos/1030/596',
+    title: 'Test the Limits',
+    client: 'Apple',
+    imageSrc: 'https://picsum.photos/id/222/1030/596',
     url: '/video-detail',
   },
   {
     videoSrc:
       'https://cartel.wiredrive.com/?routekey=iframe-embed&token=234f2dda2110b43e094f19fcde9ad4b0&loop=0&controls=1&color=4EB7D4',
-    title: 'Race Day is Still Sacred',
-    client: 'Track Smith',
-    imageSrc: 'https://picsum.photos/1030/596',
+    title: 'Test the Limits',
+    client: 'Apple',
+    imageSrc: 'https://picsum.photos/id/222/1030/596',
     url: '/video-detail',
   },
   {
     videoSrc:
       'https://cartel.wiredrive.com/?routekey=iframe-embed&token=3e2f6c9d1ebdf0816c75a7a52c5fda1c&loop=0&controls=1&color=4EB7D4',
-    title: 'Race Day is Still Sacred',
-    client: 'Track Smith',
-    imageSrc: 'https://picsum.photos/1030/596',
+    title: 'Test the Limits',
+    client: 'Apple',
+    imageSrc: 'https://picsum.photos/id/222/1030/596',
     url: '/video-detail',
   },
   {
     videoSrc:
       'https://cartel.wiredrive.com/?routekey=iframe-embed&token=300e1776e61b6928228e78cd005577ac&loop=0&controls=1&color=4EB7D4',
-    title: 'Race Day is Still Sacred',
-    client: 'Track Smith',
-    imageSrc: 'https://picsum.photos/1030/596',
+    title: 'Test the Limits',
+    client: 'Apple',
+    imageSrc: 'https://picsum.photos/id/222/1030/596',
     url: '/video-detail',
   },
   {
     videoSrc:
       'https://cartel.wiredrive.com/?routekey=iframe-embed&token=234f2dda2110b43e094f19fcde9ad4b0&loop=0&controls=1&color=4EB7D4',
-    title: 'Race Day is Still Sacred',
-    client: 'Track Smith',
-    imageSrc: 'https://picsum.photos/1030/596',
+    title: 'Test the Limits',
+    client: 'Apple',
+    imageSrc: 'https://picsum.photos/id/222/1030/596',
     url: '/video-detail',
   },
   {
     videoSrc:
       'https://cartel.wiredrive.com/?routekey=iframe-embed&token=3e2f6c9d1ebdf0816c75a7a52c5fda1c&loop=0&controls=1&color=4EB7D4',
-    title: 'Race Day is Still Sacred',
-    client: 'Track Smith',
-    imageSrc: 'https://picsum.photos/1030/596',
+    title: 'Test the Limits',
+    client: 'Apple',
+    imageSrc: 'https://picsum.photos/id/222/1030/596',
     url: '/video-detail',
   },
   {
     videoSrc:
       'https://cartel.wiredrive.com/?routekey=iframe-embed&token=300e1776e61b6928228e78cd005577ac&loop=0&controls=1&color=4EB7D4',
-    title: 'Race Day is Still Sacred',
-    client: 'Track Smith',
-    imageSrc: 'https://picsum.photos/1030/596',
+    title: 'Test the Limits',
+    client: 'Apple',
+    imageSrc: 'https://picsum.photos/id/222/1030/596',
     url: '/video-detail',
   },
   {
     videoSrc:
       'https://cartel.wiredrive.com/?routekey=iframe-embed&token=234f2dda2110b43e094f19fcde9ad4b0&loop=0&controls=1&color=4EB7D4',
-    title: 'Race Day is Still Sacred',
-    client: 'Track Smith',
-    imageSrc: 'https://picsum.photos/1030/596',
+    title: 'Test the Limits',
+    client: 'Apple',
+    imageSrc: 'https://picsum.photos/id/222/1030/596',
     url: '/video-detail',
   },
   {
     videoSrc:
       'https://cartel.wiredrive.com/?routekey=iframe-embed&token=3e2f6c9d1ebdf0816c75a7a52c5fda1c&loop=0&controls=1&color=4EB7D4',
-    title: 'Race Day is Still Sacred',
-    client: 'Track Smith',
-    imageSrc: 'https://picsum.photos/1030/596',
+    title: 'Test the Limits',
+    client: 'Apple',
+    imageSrc: 'https://picsum.photos/id/222/1030/596',
     url: '/video-detail',
   },
 ];
 
-const EditorDetailPage = () => (
+const EditorDetailOtherPage = () => (
   <Layout>
     <SEO title="Editor Detail" />
     <div className="container">
@@ -112,4 +112,4 @@ const EditorDetailPage = () => (
   </Layout>
 );
 
-export default EditorDetailPage;
+export default EditorDetailOtherPage;

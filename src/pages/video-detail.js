cb1a82d92491a3644dfee9a11f8bfef97449c3f5
Mike Riley <mikeriley131@gmail.com>
2021-05-13 00:56:53 -0600

    fixes video detail grid temp data

--- src/pages/video-detail.js
+++ src/pages/video-detail.js
@@ -7,76 +7,85 @@ import ThreeColGrid from '../components/patterns/threeColGrid';
 
 const videosList = [
   {
-    videoSrc:
-      'https://cartel.wiredrive.com/?routekey=iframe-embed&token=300e1776e61b6928228e78cd005577ac&loop=0&controls=1&color=4EB7D4',
     title: 'Race Day is Still Sacred',
     client: 'Track Smith',
-    imageSrc: 'https://picsum.photos/1030/596',
-    url: '/video-detail',
+    image: {
+      altText: 'Race Day is Still Sacred',
+      sourceUrl: 'https://picsum.photos/1060/442',
+    },
+    pagePath: '/video-detail',
   },
   {
-    videoSrc:
-      'https://cartel.wiredrive.com/?routekey=iframe-embed&token=234f2dda2110b43e094f19fcde9ad4b0&loop=0&controls=1&color=4EB7D4',
     title: 'Race Day is Still Sacred',
     client: 'Track Smith',
-    imageSrc: 'https://picsum.photos/1030/596',
-    url: '/video-detail',
+    image: {
+      altText: 'Race Day is Still Sacred',
+      sourceUrl: 'https://picsum.photos/1060/442',
+    },
+    pagePath: '/video-detail',
   },
   {
-    videoSrc:
-      'https://cartel.wiredrive.com/?routekey=iframe-embed&token=3e2f6c9d1ebdf0816c75a7a52c5fda1c&loop=0&controls=1&color=4EB7D4',
     title: 'Race Day is Still Sacred',
     client: 'Track Smith',
-    imageSrc: 'https://picsum.photos/1030/596',
-    url: '/video-detail',
+    image: {
+      altText: 'Race Day is Still Sacred',
+      sourceUrl: 'https://picsum.photos/1060/442',
+    },
+    pagePath: '/video-detail',
   },
   {
-    videoSrc:
-      'https://cartel.wiredrive.com/?routekey=iframe-embed&token=300e1776e61b6928228e78cd005577ac&loop=0&controls=1&color=4EB7D4',
     title: 'Race Day is Still Sacred',
     client: 'Track Smith',
-    imageSrc: 'https://picsum.photos/1030/596',
-    url: '/video-detail',
+    image: {
+      altText: 'Race Day is Still Sacred',
+      sourceUrl: 'https://picsum.photos/1060/442',
+    },
+    pagePath: '/video-detail',
   },
   {
-    videoSrc:
-      'https://cartel.wiredrive.com/?routekey=iframe-embed&token=234f2dda2110b43e094f19fcde9ad4b0&loop=0&controls=1&color=4EB7D4',
     title: 'Race Day is Still Sacred',
     client: 'Track Smith',
-    imageSrc: 'https://picsum.photos/1030/596',
-    url: '/video-detail',
+    image: {
+      altText: 'Race Day is Still Sacred',
+      sourceUrl: 'https://picsum.photos/1060/442',
+    },
+    pagePath: '/video-detail',
   },
   {
-    videoSrc:
-      'https://cartel.wiredrive.com/?routekey=iframe-embed&token=3e2f6c9d1ebdf0816c75a7a52c5fda1c&loop=0&controls=1&color=4EB7D4',
     title: 'Race Day is Still Sacred',
     client: 'Track Smith',
-    imageSrc: 'https://picsum.photos/1030/596',
-    url: '/video-detail',
+    image: {
+      altText: 'Race Day is Still Sacred',
+      sourceUrl: 'https://picsum.photos/1060/442',
+    },
+    pagePath: '/video-detail',
   },
   {
-    videoSrc:
-      'https://cartel.wiredrive.com/?routekey=iframe-embed&token=300e1776e61b6928228e78cd005577ac&loop=0&controls=1&color=4EB7D4',
     title: 'Race Day is Still Sacred',
     client: 'Track Smith',
-    imageSrc: 'https://picsum.photos/1030/596',
-    url: '/video-detail',
+    image: {
+      altText: 'Race Day is Still Sacred',
+      sourceUrl: 'https://picsum.photos/1060/442',
+    },
+    pagePath: '/video-detail',
   },
   {
-    videoSrc:
-      'https://cartel.wiredrive.com/?routekey=iframe-embed&token=234f2dda2110b43e094f19fcde9ad4b0&loop=0&controls=1&color=4EB7D4',
     title: 'Race Day is Still Sacred',
     client: 'Track Smith',
-    imageSrc: 'https://picsum.photos/1030/596',
-    url: '/video-detail',
+    image: {
+      altText: 'Race Day is Still Sacred',
+      sourceUrl: 'https://picsum.photos/1060/442',
+    },
+    pagePath: '/video-detail',
   },
   {
-    videoSrc:
-      'https://cartel.wiredrive.com/?routekey=iframe-embed&token=3e2f6c9d1ebdf0816c75a7a52c5fda1c&loop=0&controls=1&color=4EB7D4',
     title: 'Race Day is Still Sacred',
     client: 'Track Smith',
-    imageSrc: 'https://picsum.photos/1030/596',
-    url: '/video-detail',
+    image: {
+      altText: 'Race Day is Still Sacred',
+      sourceUrl: 'https://picsum.photos/1060/442',
+    },
+    pagePath: '/video-detail',
   },
 ];
 

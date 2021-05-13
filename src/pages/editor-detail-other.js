d3bcdecbd4da95df1deb4397b3300092dff9d6e1
Mike Riley <mikeriley131@gmail.com>
2021-05-13 00:19:32 -0600

    fixes temp editor detail data

--- src/pages/editor-detail-other.js
+++ src/pages/editor-detail-other.js
@@ -9,73 +9,73 @@ const videosList = [
   {
     videoSrc:
       'https://cartel.wiredrive.com/?routekey=iframe-embed&token=300e1776e61b6928228e78cd005577ac&loop=0&controls=1&color=4EB7D4',
-    title: 'Test the Limits',
-    client: 'Apple',
-    imageSrc: 'https://picsum.photos/id/222/1030/596',
+    title: 'Race Day is Still Sacred',
+    client: 'Track Smith',
+    imageSrc: 'https://picsum.photos/1030/596',
     url: '/video-detail',
   },
   {
     videoSrc:
       'https://cartel.wiredrive.com/?routekey=iframe-embed&token=234f2dda2110b43e094f19fcde9ad4b0&loop=0&controls=1&color=4EB7D4',
-    title: 'Test the Limits',
-    client: 'Apple',
-    imageSrc: 'https://picsum.photos/id/222/1030/596',
+    title: 'Race Day is Still Sacred',
+    client: 'Track Smith',
+    imageSrc: 'https://picsum.photos/1030/596',
     url: '/video-detail',
   },
   {
     videoSrc:
       'https://cartel.wiredrive.com/?routekey=iframe-embed&token=3e2f6c9d1ebdf0816c75a7a52c5fda1c&loop=0&controls=1&color=4EB7D4',
-    title: 'Test the Limits',
-    client: 'Apple',
-    imageSrc: 'https://picsum.photos/id/222/1030/596',
+    title: 'Race Day is Still Sacred',
+    client: 'Track Smith',
+    imageSrc: 'https://picsum.photos/1030/596',
     url: '/video-detail',
   },
   {
     videoSrc:
       'https://cartel.wiredrive.com/?routekey=iframe-embed&token=300e1776e61b6928228e78cd005577ac&loop=0&controls=1&color=4EB7D4',
-    title: 'Test the Limits',
-    client: 'Apple',
-    imageSrc: 'https://picsum.photos/id/222/1030/596',
+    title: 'Race Day is Still Sacred',
+    client: 'Track Smith',
+    imageSrc: 'https://picsum.photos/1030/596',
     url: '/video-detail',
   },
   {
     videoSrc:
       'https://cartel.wiredrive.com/?routekey=iframe-embed&token=234f2dda2110b43e094f19fcde9ad4b0&loop=0&controls=1&color=4EB7D4',
-    title: 'Test the Limits',
-    client: 'Apple',
-    imageSrc: 'https://picsum.photos/id/222/1030/596',
+    title: 'Race Day is Still Sacred',
+    client: 'Track Smith',
+    imageSrc: 'https://picsum.photos/1030/596',
     url: '/video-detail',
   },
   {
     videoSrc:
       'https://cartel.wiredrive.com/?routekey=iframe-embed&token=3e2f6c9d1ebdf0816c75a7a52c5fda1c&loop=0&controls=1&color=4EB7D4',
-    title: 'Test the Limits',
-    client: 'Apple',
-    imageSrc: 'https://picsum.photos/id/222/1030/596',
+    title: 'Race Day is Still Sacred',
+    client: 'Track Smith',
+    imageSrc: 'https://picsum.photos/1030/596',
     url: '/video-detail',
   },
   {
     videoSrc:
       'https://cartel.wiredrive.com/?routekey=iframe-embed&token=300e1776e61b6928228e78cd005577ac&loop=0&controls=1&color=4EB7D4',
-    title: 'Test the Limits',
-    client: 'Apple',
-    imageSrc: 'https://picsum.photos/id/222/1030/596',
+    title: 'Race Day is Still Sacred',
+    client: 'Track Smith',
+    imageSrc: 'https://picsum.photos/1030/596',
     url: '/video-detail',
   },
   {
     videoSrc:
       'https://cartel.wiredrive.com/?routekey=iframe-embed&token=234f2dda2110b43e094f19fcde9ad4b0&loop=0&controls=1&color=4EB7D4',
-    title: 'Test the Limits',
-    client: 'Apple',
-    imageSrc: 'https://picsum.photos/id/222/1030/596',
+    title: 'Race Day is Still Sacred',
+    client: 'Track Smith',
+    imageSrc: 'https://picsum.photos/1030/596',
     url: '/video-detail',
   },
   {
     videoSrc:
       'https://cartel.wiredrive.com/?routekey=iframe-embed&token=3e2f6c9d1ebdf0816c75a7a52c5fda1c&loop=0&controls=1&color=4EB7D4',
-    title: 'Test the Limits',
-    client: 'Apple',
-    imageSrc: 'https://picsum.photos/id/222/1030/596',
+    title: 'Race Day is Still Sacred',
+    client: 'Track Smith',
+    imageSrc: 'https://picsum.photos/1030/596',
     url: '/video-detail',
   },
 ];

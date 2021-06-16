6790c7e6ef60b69ca9e9fc0e616852c48e83c075
Mike Riley <mikeriley131@gmail.com>
2021-06-15 23:59:54 -0600

    temp data

--- src/pages/video-detail.js
+++ src/pages/video-detail.js
@@ -5,6 +5,21 @@ import SEO from '../components/seo';
 import { VideoDetail } from '../components/patterns/videoDetail';
 import ThreeColGrid from '../components/patterns/threeColGrid';
 
+const videoDetail = {
+  editor: 'Andy McKay',
+  videoStill: {
+    altText: 'The alternative text.',
+    sourceUrl: 'https://placeimg.com/750/422/any',
+  },
+  videoUrl:
+    'https://cartel.wiredrive.com/?routekey=iframe-embed&token=d98f93883fd6f20f8fc38fa44d76e06b&autoplay=1&loop=0&controls=1&color=4EB7D4',
+  title: 'The Title',
+  director: 'Homer Simpson',
+  agency: 'CPB',
+  productionCompany: 'HBO',
+  duration: '01:00',
+};
+
 const videosList = [
   {
     title: 'Race Day is Still Sacred',
@@ -92,7 +107,7 @@ const videosList = [
 const VideoDetailPage = () => (
   <Layout>
     <SEO title="Video Detail" />
-    <VideoDetail />
+    <VideoDetail data={videoDetail} />
     <div className="container">
       <ThreeColGrid list={videosList} />
     </div>

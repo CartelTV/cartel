483491d769e9888b02998f22f33078e080834897
Mike Riley <mikeriley131@gmail.com>
2021-06-26 15:45:21 -0600

    temp 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'gatsby';
 
-import { Layout } from '../components/layout'
-import SEO from '../components/seo'
+import { Layout } from '../components/layout';
+import SEO from '../components/seo';
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="container">
+      <h1>Sorry</h1>
+      <p>
+        It seems you&apos;ve ended up on a page that does not exist.{' '}
+        <Link to="/work">Click here to get back on track</Link>.
+      </p>
+      <br />
+    </div>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
 
 // or if you just want to redirect to home page:
 // const NotFoundPage = () => {

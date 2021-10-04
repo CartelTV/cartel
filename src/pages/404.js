c74f820b8b98063c362a20367c3f80a7edd862b9
Mike Riley <mikeriley131@gmail.com>
2021-10-04 16:13:15 -0600

    only adds darkened overlay on hover

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,25 +1,3 @@
-// import React from 'react';
-// import { Link } from 'gatsby';
-
-// import { Layout } from '../components/layout';
-// import SEO from '../components/seo';
-
-// const NotFoundPage = () => (
-//   <Layout>
-//     <SEO title="404: Not found" />
-//     <div className="container">
-//       <h1>Sorry</h1>
-//       <p>
-//         It seems you&apos;ve ended up on a page that does not exist.{' '}
-//         <Link to="/work">Click here to get back on track</Link>.
-//       </p>
-//       <br />
-//     </div>
-//   </Layout>
-// );
-
-// export default NotFoundPage;
-
 const NotFoundPage = () => {
   if (typeof window !== 'undefined') {
     window.location = '/';

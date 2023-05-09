776312b848da135d25d2369cf4519e9fd1d620da
Mike Riley <mikeriley131@gmail.com>
2023-05-08 21:59:18 -0600

    updates contact page layout

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,20 +1,18 @@
 /* eslint-disable no-new */
-import React, { Fragment, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
-import { Helmet } from 'react-helmet';
 
 import { Layout } from '../components/layout';
 import SEO from '../components/seo';
 
-import iconMapMarker from '../images/map-pin.png';
 import iconFacebook from '../images/icon-facebook.svg';
 import iconInstagram from '../images/icon-instagram.svg';
 
 const ContactPage = ({ data, location }) => {
   const contactData = data.allWpPage.edges[0].node.contactPage;
   const {
-    contactAddress,
+    contactAddresses,
     contactPhoneNumber,
     contactFacebookUrl,
     contactInstagramUrl,
@@ -22,111 +20,47 @@ const ContactPage = ({ data, location }) => {
     contactRepresentation,
   } = contactData;
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      window.initMap = () => {
-        const map = new window.google.maps.Map(document.getElementById('map'), {
-          disableDefaultUI: true,
-          center: {
-            lat: 34.03314675929526,
-            lng: -118.46453928460603,
-          },
-          zoom: 14,
-          styles: [
-            {
-              featureType: 'road',
-              elementType: 'geometry',
-              stylers: [
-                { visibility: 'simplified' },
-                { color: '#b4b9bd' },
-                { lightness: 15 },
-              ],
-            },
-            {
-              featureType: 'transit.line',
-              stylers: [
-                { lightness: 37 },
-                { color: '#5e6871' },
-                { visibility: 'simplified' },
-              ],
-            },
-            {
-              featureType: 'water',
-              stylers: [{ color: '#3d4b56' }, { invert_lightness: true }],
-            },
-            {
-              featureType: 'road',
-              elementType: 'labels.text',
-              stylers: [{ visibility: 'simplified' }, { color: '#10253a' }],
-            },
-            {
-              featureType: 'poi',
-              elementType: 'geometry',
-              stylers: [
-                { visibility: 'simplified' },
-                { color: '#b0b5b8' },
-                { lightness: 68 },
-              ],
-            },
-            {
-              featureType: 'landscape',
-              stylers: [
-                { visibility: 'simplified' },
-                { color: '#ebecee' },
-                { lightness: -7 },
-              ],
-            },
-            {
-              featureType: 'administrative',
-              elementType: 'labels.text',
-              stylers: [{ visibility: 'simplified' }, { color: '#10253a' }],
-            },
-            {
-              featureType: 'transit',
-              elementType: 'geometry',
-              stylers: [
-                { visibility: 'on' },
-                { color: '#7c8089' },
-                { lightness: 48 },
-              ],
-            },
-            {
-              featureType: 'transit',
-              elementType: 'labels.text',
-              stylers: [{ visibility: 'simplified' }, { color: '#10253a' }],
-            },
-            {
-              featureType: 'poi',
-              elementType: 'labels.text',
-              stylers: [{ visibility: 'simplified' }, { color: '#10253a' }],
-            },
-          ],
-        });
-
-        new window.google.maps.Marker({
-          position: { lat: 34.03314675929526, lng: -118.46453928460603 },
-          icon: iconMapMarker,
-          map,
-        });
-      };
-    }
-  }, []);
-
   return (
-    <Fragment>
-      <Helmet>
-        <script
-          async
-          defer
-          src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCSQ-2TUSm0yqVfuotzZB44vTTHDQkeUNA&callback=initMap"
-        />
-      </Helmet>
-      <Layout location={location}>
-        <SEO title="Contact" />
-        <article className="contact">
-          <div className="container">
-            <div className="contact__col">
-              <div id="map" />
+    <Layout location={location}>
+      <SEO title="Contact" />
+      <article className="contact">
+        <div className="container">
+          <h1>Contact</h1>
+          <div className="contact__grid">
+            <div className="contact__grid-column">
+              <h2 className="contact__list-heading">Team</h2>
+              <ul className="contact__team-list">
+                {contactPersons.map(person => (
+                  <li
+                    className="contact__team-item contact__block"
+                    style={
+                      contactPersons.length === 1
+                        ? { marginBottom: '7.25rem' }
+                        : null
+                    }
+                    key={person.contactPersonName}
+                  >
+                    <p>
+                      <strong>{person.contactPersonName}</strong>
+                      <br />
+                      {person.contactPersonTitle}
+                      <br />
+                      <a href={`mailto:${person.contactPersonEmail}`}>
+                        {person.contactPersonEmail}
+                      </a>
+                      <br />
+                      <a
+                        href={`tel:1${person.contactPersonPhone
+                          .replace(/–/g, '')
+                          .replace(/-/g, '')}`}
+                      >
+                        {person.contactPersonPhone}
+                      </a>
+                    </p>
+                  </li>
+                ))}
+              </ul>
+
               <ul className="contact__social-list">
                 {contactInstagramUrl && (
                   <li className="contact__social-item">
@@ -157,17 +91,14 @@ const ContactPage = ({ data, location }) => {
                   </li>
                 )}
               </ul>
-
-              <address className="contact__address">{contactAddress}</address>
             </div>
 
-            <div className="contact__col">
-              <h1>Contact</h1>
-
-              <div className="contact__group">
-                <div className="contact__block">
+            <div className="contact__grid-column">
+              <div className="contact__enquiries-group">
+                <div className="contact__enquiries-group-heading">
                   <h2>Enquiries</h2>
                   <a
+                    className="contact__enquiries-phone-number"
                     href={`tel:1${contactPhoneNumber
                       .replace(/–/g, '')
                       .replace(/-/g, '')}`}
@@ -176,115 +107,58 @@ const ContactPage = ({ data, location }) => {
                   </a>
                 </div>
 
-                {contactPersons
-                  .filter(
-                    person => person.contactPersonTitle === 'Managing Director'
-                  )
-                  .map(person => (
-                    <div
-                      className="contact__block"
-                      style={
-                        contactPersons.length === 1
-                          ? { marginBottom: '7.25rem' }
-                          : null
-                      }
-                      key={person.contactPersonName}
-                    >
-                      <p>
-                        <strong>{person.contactPersonName}</strong>
+                {contactAddresses.map(address => (
+                  <address className="contact__address">
+                    <span className="contact__address-title">
+                      {address.contactAddressHeading}
+                    </span>
+                    <br />
+                    <span className="contact__address-street">
+                      {address.contactAddressStreet}
+                    </span>
+                    <br />
+                    <span className="contact__address-city">
+                      {address.contactAddressCityStateZip}
+                    </span>
+                  </address>
+                ))}
+              </div>
+            </div>
+
+            <div className="contact__grid-column">
+              <h2 className="contact__list-heading">Representation</h2>
+              <ul className="contact__representation-list">
+                {contactRepresentation.map(group => (
+                  <li
+                    className="contact__block"
+                    key={group.representationGroupName}
+                  >
+                    <h3>{group.representationGroupName}</h3>
+                    {group.representationContactPersons.map(person => (
+                      <p key={person.representationContactName}>
+                        {person.representationContactName}
                         <br />
-                        {person.contactPersonTitle}
-                        <br />
-                        <a href={`mailto:${person.contactPersonEmail}`}>
-                          {person.contactPersonEmail}
+                        <a href={`mailto:${person.representationContactEmail}`}>
+                          {person.representationContactEmail}
                         </a>
                         <br />
                         <a
-                          href={`tel:1${person.contactPersonPhone
+                          href={`tel:1${person.representationContactPhone
                             .replace(/–/g, '')
                             .replace(/-/g, '')}`}
                         >
-                          {person.contactPersonPhone}
+                          {person.representationContactPhone}
                         </a>
                       </p>
-                    </div>
-                  ))}
-
-                <div className="contact__sub-group">
-                  {contactPersons
-                    .filter(
-                      person =>
-                        person.contactPersonTitle !== 'Managing Director'
-                    )
-                    .map(person => (
-                      <div
-                        className="contact__block"
-                        style={
-                          contactPersons.length === 1
-                            ? { marginBottom: '7.25rem' }
-                            : null
-                        }
-                        key={person.contactPersonName}
-                      >
-                        <p>
-                          <strong>{person.contactPersonName}</strong>
-                          <br />
-                          {person.contactPersonTitle}
-                          <br />
-                          <a href={`mailto:${person.contactPersonEmail}`}>
-                            {person.contactPersonEmail}
-                          </a>
-                          <br />
-                          <a
-                            href={`tel:1${person.contactPersonPhone
-                              .replace(/–/g, '')
-                              .replace(/-/g, '')}`}
-                          >
-                            {person.contactPersonPhone}
-                          </a>
-                        </p>
-                      </div>
                     ))}
-                </div>
-              </div>
-
-              <div className="contact__group">
-                <h2>Representation</h2>
-                <div className="contact__representation-wrapper">
-                  {contactRepresentation.map(group => (
-                    <div
-                      className="contact__block"
-                      key={group.representationGroupName}
-                    >
-                      <h3>{group.representationGroupName}</h3>
-                      {group.representationContactPersons.map(person => (
-                        <p key={person.representationContactName}>
-                          {person.representationContactName}
-                          <br />
-                          <a
-                            href={`mailto:${person.representationContactEmail}`}
-                          >
-                            {person.representationContactEmail}
-                          </a>
-                          <br />
-                          <a
-                            href={`tel:1${person.representationContactPhone
-                              .replace(/–/g, '')
-                              .replace(/-/g, '')}`}
-                          >
-                            {person.representationContactPhone}
-                          </a>
-                        </p>
-                      ))}
-                    </div>
-                  ))}
-                </div>
-              </div>
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
-        </article>
-      </Layout>
-    </Fragment>
+        </div>
+      </article>
+    </Layout>
   );
 };
 
@@ -311,7 +185,11 @@ export const query = graphql`
             }
             contactInstagramUrl
             contactFacebookUrl
-            contactAddress
+            contactAddresses {
+              contactAddressHeading
+              contactAddressStreet
+              contactAddressCityStateZip
+            }
           }
         }
       }
@@ -326,7 +204,7 @@ ContactPage.propTypes = {
         PropTypes.shape({
           node: PropTypes.shape({
             contactPage: PropTypes.shape({
-              contactAddress: PropTypes.string,
+              contactAddresses: PropTypes.arrayOf(PropTypes.shape({})),
               contactFacebookUrl: PropTypes.string,
               contactInstagramUrl: PropTypes.string,
               contactPersons: PropTypes.arrayOf(PropTypes.shape({})),
